8ae9a4ffba447713afbac44c16ffb4f406280f08
PradeepaVijay <88672903+PradeepaVijay@users.noreply.github.com>
2022-04-22 12:48:41 +0400

    Add files via upload

---
+++ src/redux/actions.js
@@ -0,0 +1,29 @@
+import {
+  REQUEST_MOVIES,
+  REQUEST_MOVIE_DETAILS,
+  REQUEST_SEARCH,
+  FETCH_TOPRATED
+  /*RESET_ERROR_FETCH,*/
+} from "./config";
+
+export const requestMovies = () => {
+  return { type: REQUEST_MOVIES };
+};
+export const fetchedTopRated = () => {
+  return { type: FETCH_TOPRATED };
+};
+
+
+export const fetchMovieDetails = (id) => {
+  return { type: REQUEST_MOVIE_DETAILS, id };
+};
+
+/*someday i'll figure out, how to reset the error properly, but for now user must reload the page*/
+
+/* export function resetError() {   
+  return { type: RESET_ERROR_FETCH };
+} */
+
+export const requestSearch = (query, isScrolling) => {
+  return { type: REQUEST_SEARCH, query, isScrolling };
+};
---
+++ src/redux/sagas.js
@@ -0,0 +1,119 @@
+import { takeEvery, put, call, select } from "redux-saga/effects";
+import {
+  REQUEST_MOVIES,
+  API_KEY,
+  REQUEST_MOVIE_DETAILS,
+  REQUEST_SEARCH,
+  FETCH_TOPRATED
+} from "./config";
+import { showLoader, hideLoader } from "./reducers/actions/appActions";
+import {
+  actionFetchMovieDetails,
+  actionFetchMovies,
+  fetchGenres,
+  fetchSimilars,
+  clearBestMovies,
+  fetchTopRated,
+} from "./reducers/actions/movieActions";
+import * as selector from "./selectors";
+import { fetchError, receiveTotalPages } from "./reducers/actions/appActions";
+import {
+  fetchSearch,
+  clearSearchedMovies,
+} from "./reducers/actions/searchActions";
+
+export function* sagaWatcher() {
+  /* catch every action that have type REQUEST_MOVIES, etc and apply a function to it*/
+  yield takeEvery(REQUEST_MOVIES, fetchMoviesWorker);
+  yield takeEvery(REQUEST_MOVIE_DETAILS, fetchMovieDetailsWorker);
+  yield takeEvery(REQUEST_SEARCH, fetchSearchWorker);
+  yield takeEvery(FETCH_TOPRATED, fetchTopRatedMovies);
+}
+function* fetchTopRatedMovies() {
+  try {
+    const page = yield select(selector.page);
+    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_SECRET_API_KEY}&language=en-US&page=${page}`;
+    const payload = yield call(fetchMovies, url);
+    yield put(clearSearchedMovies());
+    yield put(actionFetchMovies(payload.results, payload.page));
+    yield put(receiveTotalPages(payload.total_pages));
+  } catch (e) {
+    yield put(fetchError());
+    /*  console.log(e); */
+  }
+}
+function* fetchMoviesWorker() {
+  try {
+    const page = yield select(selector.page);
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}
+    `;
+    const payload = yield call(fetchMovies, url);
+    yield put(clearSearchedMovies());
+    yield put(actionFetchMovies(payload.results, payload.page));
+    yield put(receiveTotalPages(payload.total_pages));
+  } catch (e) {
+    yield put(fetchError());
+    /*  console.log(e); */
+  }
+}
+
+function* fetchSearchWorker({ query = "", isScrolling }) {
+  try {
+    if (query) {
+      let page;
+      const search_page = yield select(selector.search_page);
+      const scroll_page = yield select(selector.scroll_page);
+      isScrolling ? (page = scroll_page) : (page = search_page);
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`;
+      const payload = yield call(fetchMovies, url);
+      yield put(clearBestMovies());
+      yield put(fetchSearch(query, isScrolling, payload.results, payload.page));
+      yield put(receiveTotalPages(payload.total_pages));
+    }
+  } catch (e) {
+    yield put(fetchError());
+  }
+}
+
+function* fetchMovieDetailsWorker({ id }) {
+  try {
+    yield put(showLoader());
+
+    const details = yield call(fetchMovieDetails, id);
+
+    yield put(actionFetchMovieDetails(details));
+    yield put(fetchGenres(details.genres));
+
+    const genreIds = details.genres.map((genre) => genre.id);
+    const similars = yield call(fetchMovieSimilars, genreIds);
+
+    yield put(fetchSimilars(similars));
+
+    yield put(hideLoader());
+  } catch (e) {
+    yield put(fetchError());
+  }
+}
+
+async function fetchMovies(url) {
+  const data = await fetch(url);
+  const movies = await data.json();
+  return await movies;
+}
+
+async function fetchMovieDetails(id) {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
+  );
+  const details = await data.json();
+  return await details;
+}
+
+async function fetchMovieSimilars(genreIds) {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&with_genres=${genreIds}&include_adult=false&include_video=false&page=1`
+  );
+  const similars = await data.json();
+  similars.results.length = 14; //14 similar movie cards looks nice
+  return await similars.results;
+}
